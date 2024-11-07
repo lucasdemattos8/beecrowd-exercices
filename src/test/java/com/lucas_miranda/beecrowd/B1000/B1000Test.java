29758d29fd1727165da5dd8910967e1cbb7315bb
Lucas de Mattos Miranda <ldemattosmiranda@gmail.com>
2024-11-07 10:29:08 -0300

    #4 - Removed unnecessary comments and DisplayName translated to English-US language

--- src/test/java/com/lucas_miranda/beecrowd/B1000/B1000Test.java
+++ src/test/java/com/lucas_miranda/beecrowd/B1000/B1000Test.java
@@ -11,26 +11,21 @@ import java.io.PrintStream;
 class B1000Test {
 
     @Test
-    @DisplayName("Testar se a saída corresponde ao valor 'Hello World'")
+    @DisplayName("Should test if the Output corresponds 'Hello World'")
     public void shouldPrintHelloWorldToConsole() {
-        // Redireciona o System.out para capturar a saída
+
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         PrintStream printStream = new PrintStream(outputStream);
         
-        // Redireciona o System.out
         System.setOut(printStream);
         
-        // Chama o método main ou o método que gera a saída
         Program.main(new String[]{});
         
-        // Restaura o System.out
         System.setOut(System.out);
         
-        // Verifica a saída (sem considerar espaços em branco ou quebras de linha)
         assertEquals("Hello, World!\n", outputStream.toString().replaceAll("\\r\\n|\\r|\\n", "\n"));
     }
 
 
-    // outros testes
 }
 
